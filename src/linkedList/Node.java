a64d616b30bcb1b9a1dd15663276644cf3754ebc
5466315@qq.com <h1116761886>
2019-08-22 17:19:26 +0800

    code

---
+++ src/linkedList/Node.java
@@ -0,0 +1,19 @@
+package linkedList;
+
+public class Node<T> {
+    T t;
+    Node next;
+
+    public Node(T t, Node next){
+        this.t = t;
+        this.next = next;
+    }
+
+    public Node() {
+        this(null, null);
+    }
+
+    public Node(T t){
+        this(t, null);
+    }
+}
